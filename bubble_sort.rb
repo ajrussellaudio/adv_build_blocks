0e8b3d4cfd2fe55e305536ee8def13f4500d5b84
ajrussellaudio <ajrussellaudio@gmail.com>
2014-05-26 15:04:46 +0100

    updated bubble_sort, bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,33 @@
-class Array
-
-	def bubble_sort(array)
-		for pass in 1..array.length
-			array.each_with_index do |item, i|
-				if array[i+1] && array[i] > array[i+1]
-					array.swap_items(i)
-				end
+def bubble_sort(array)
+	n = array.length
+	swapped = true
+	until !swapped do
+		swapped = false
+		for i in 1...n do
+			# if this pair is out of order
+			if array[i-1] > array[i]
+				# swap them and remember something changed
+				array[i-1], array[i] = array[i], array[i-1]
+				swapped = true
 			end
 		end
-		array
 	end
-
-	def swap_items(i)
-		self[i], self[i+1] = self[i+1], self[i]
-	end
-
+	array
 end
+
+def bubble_sort_by(array)
+	n = array.length
+	swapped = true
+	until !swapped do
+		swapped = false
+		for i in 1...n do
+			sort = yield(array[i-1], array[i])
+			if sort < 0
+				array[i-1], array[i] = array[i], array[i-1]
+				swapped = true
+			end
+		end
+	end
+	array	
+end
+
